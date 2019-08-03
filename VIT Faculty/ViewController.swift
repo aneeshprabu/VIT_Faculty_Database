773358e0c16d6d34772e2ce49afb3621330fff15
Scorpio_2007 <aneeshprabu@gmail.com>
2019-08-03 22:11:18 +0530

    Initial Commit

---
+++ VIT Faculty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIT Faculty
+//
+//  Created by Aneesh Prabu on 03/08/19.
+//  Copyright © 2019 Aneesh Prabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
